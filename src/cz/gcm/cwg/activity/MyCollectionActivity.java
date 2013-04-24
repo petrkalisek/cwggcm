2042b74f18a2e2fbe5c5fe730924e4e5ca79467d
Petr Kalisek <petr.kalisek@gmail.com>
2013-04-24 15:21:21 +0200

    Fix db

--- src/cz/gcm/cwg/activity/MyCollectionActivity.java
+++ src/cz/gcm/cwg/activity/MyCollectionActivity.java
@@ -5,8 +5,10 @@ import org.json.JSONObject;
 
 import android.app.ProgressDialog;
 import android.content.ContentValues;
+import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
@@ -22,90 +24,19 @@ import cz.gcm.cwg.layouts.SimpleListItem;
 public class MyCollectionActivity extends BaseActivity {
 
 	private ProgressDialog progressDialog;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_my_collection);
-		
+
 	}
-	
+
 	@Override
-	protected void onStart(){
+	protected void onStart() {
 		super.onStart();
-		
-		
-		ListView listenersList = (ListView) findViewById(R.id.cwgList);
-		try {
-			MyCollection myCollection = new MyCollection();
-			AsyncTaskActivity Async = new AsyncTaskActivity();
-
-			try {
-				JSONObject myCollectionResult = Async.execute(myCollection)
-						.get();
-				
-				Log.d("MyCollectionActivity::jsonResult",
-						myCollectionResult.toString());
-
-				if (myCollectionResult.optJSONArray("Export").length() > 0) {
-					JSONArray exportArray = myCollectionResult
-							.getJSONArray("Export");
-					
-					Cwg cwg = new Cwg(this);
-					
-					for (int i = 0; i < exportArray.length(); i++) {
-						JSONObject t = (JSONObject) exportArray.get(i);
-						JSONArray collection = t.optJSONArray("collection");
-						if (collection != null) {
-							Log.i("MyCollectionActivity",
-									"ID:" + t.getInt("id"));
-							Log.i("MyCollectionActivity", "collection:"
-									+ collection.toString());
-						} else {
-							Log.i("MyCollectionActivity", "NOT COLLECTION ID:"
-									+ t.getInt("id"));
-						}
-						
-						//TODO: udelat nejak insert ale i jako update, kdyz znam ID
-						ContentValues values = new ContentValues();
-						values.put(Cwg.COLUMN_ID, t.optString("id"));
-						values.put(Cwg.COLUMN_NAME, t.optString("name"));
-						values.put(Cwg.COLUMN_CWGNO, t.optString("cwgno"));
-						values.put(Cwg.COLUMN_VERSION, t.optString("version"));
-						values.put(Cwg.COLUMN_IMAGE, t.optString("image"));
-						
-						long id = 0;
-						if( cwg.getCwg(t.getInt("id")).getCount() > 0){
-							id = cwg.updateCwg(t.getInt("id"), values);
-							Log.i("MyCollectionActivity", "update database:"
-									+ String.valueOf(id));
-						}else{
-							id = cwg.addCwg(values);
-							Log.i("MyCollectionActivity", "add database:"
-									+ String.valueOf(id));
-						}
-						
-						
-						
-						
-					}
-					Cursor c = cwg.getAllCwg();
-					startManagingCursor(c);
-					
-					listenersList.setAdapter(new SimpleListItem(this, c ));
-				}
-				
-				
-
-			} catch (Exception e) {
-				Log.w("MyCollectionActivity",
-						"Async.execute exception:" + e.toString());
-			}
-		} catch (Exception e) {
-
-		}
-
+		loadData(false);
 	}
 
 	@Override
@@ -114,10 +45,88 @@ public class MyCollectionActivity extends BaseActivity {
 		getMenuInflater().inflate(R.menu.my_cwg, menu);
 		return true;
 	}
-	
-	
-	public void refreshData(View view){
-		Toast.makeText(getApplicationContext(), "refreshData", Toast.LENGTH_LONG).show();
+
+	public void refreshData(View view) {
+		Toast.makeText(getApplicationContext(), "refreshData",
+				Toast.LENGTH_LONG).show();
+		loadData(true);
+	}
+
+	private void loadData(Boolean force) {
+		SharedPreferences prefsRead = PreferenceManager
+				.getDefaultSharedPreferences(getApplicationContext());
+
+		Boolean useCacheData = prefsRead.getBoolean("pref_useCacheData", false);
+		Log.d("useCacheData", useCacheData.toString());
+
+		Cwg cwg = new Cwg(this);
+
+		if (!useCacheData || force) {
+			try {
+				MyCollection myCollection = new MyCollection();
+				AsyncTaskActivity Async = new AsyncTaskActivity();
+
+				try {
+					JSONObject myCollectionResult = Async.execute(myCollection)
+							.get();
+
+					Log.d("MyCollectionActivity::jsonResult",
+							myCollectionResult.toString());
+
+					if (myCollectionResult.optJSONArray("Export").length() > 0) {
+						JSONArray exportArray = myCollectionResult
+								.getJSONArray("Export");
+
+						for (int i = 0; i < exportArray.length(); i++) {
+							JSONObject t = (JSONObject) exportArray.get(i);
+							JSONArray collection = t.optJSONArray("collection");
+							if (collection != null) {
+								Log.i("MyCollectionActivity",
+										"ID:" + t.getInt("id"));
+								Log.i("MyCollectionActivity", "collection:"
+										+ collection.toString());
+							} else {
+								Log.i("MyCollectionActivity",
+										"NOT COLLECTION ID:" + t.getInt("id"));
+							}
+
+							// TODO: udelat nejak insert ale i jako update, kdyz
+							// znam ID
+							ContentValues values = new ContentValues();
+							values.put(Cwg.COLUMN_ID, t.optString("id"));
+							values.put(Cwg.COLUMN_NAME, t.optString("name"));
+							values.put(Cwg.COLUMN_CWGNO, t.optString("cwgno"));
+							values.put(Cwg.COLUMN_VERSION,
+									t.optString("version"));
+							values.put(Cwg.COLUMN_IMAGE, t.optString("image"));
+
+							long id = 0;
+							if (cwg.getCwg(t.getInt("id")).getCount() > 0) {
+								id = cwg.updateCwg(t.getInt("id"), values);
+								Log.i("MyCollectionActivity",
+										"update database:" + String.valueOf(id));
+							} else {
+								id = cwg.addCwg(values);
+								Log.i("MyCollectionActivity", "add database:"
+										+ String.valueOf(id));
+							}
+						}
+					}
+
+				} catch (Exception e) {
+					Log.w("MyCollectionActivity", "Async.execute exception:"
+							+ e.toString());
+				}
+			} catch (Exception e) {
+
+			}
+		}
+
+		ListView listenersList = (ListView) findViewById(R.id.cwgList);
+		Cursor c = cwg.getAllCwg();
+		startManagingCursor(c);
+		listenersList.setAdapter(new SimpleListItem(this, c));
+		cwg.close();
 	}
 
 }
