b42afbd1a2da3c56ac93e518bb033ae7c2f79888
Petr Kalisek <petr.kalisek@gmail.com>
2013-04-30 10:39:21 +0200

    Jen design detail

--- src/cz/gcm/cwg/activity/DetailCwgActivity.java
+++ src/cz/gcm/cwg/activity/DetailCwgActivity.java
@@ -2,15 +2,25 @@ package cz.gcm.cwg.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.widget.TextView;
 import cz.gcm.cwg.R;
 
 public class DetailCwgActivity extends Activity {
 
+	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_detail_cwg);
+		Bundle extras = getIntent().getExtras();
+		
+		long cwgId = extras.getLong("id");
+		
+		TextView textViewId = (TextView) findViewById(R.id.cwgId);
+		textViewId.setText(""+cwgId);
 	}
 
 	@Override
